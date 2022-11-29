9d01f2b8a7e2e30860826f0ca92022fb011277e5
Barros42 <mdbf42@gmail.com>
2022-11-29 20:59:57 -0300

    UPDATE @ Comments on conexao.php

--- modulos/conexao.php
+++ modulos/conexao.php
@@ -1,11 +1,14 @@
 <?php 
-
+# Servidor do Banco
 $hostname = "localhost";
+# Usuário do Banco
 $username = "root";
+# Senha do Banco
 $password = "";
+# Nome do Banco de Dados
 $database = "vapor";
+# Porta do Banco de Dados
 $port = 3306;
-
+# Objeto de conexão
 $conn = mysqli_connect($hostname, $username, $password, $database, $port);
-
 ?>
